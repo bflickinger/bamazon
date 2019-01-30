7d4326811d78cf77bdbf720a5814c9c0bfd6ccaf
bflickinger <barry.flickinger@gmail.com>
2019-01-30 11:49:30 -0700

    ongoing progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,18 +2,54 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'MS@yrk00n1969',
-    database : 'bamazon'
+  host: 'localhost',
+  user: 'root',
+  password: 'P4ssw0rd',
+  database: 'bamazon'
 });
 
-connection.connect();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  startProcess();
+});
 
-connection.query('SELECT * FROM bamazon.products', function (error, results, fields) {
+function startProcess() {
+  buyProducts()
+}
+
+function printAll() {
+  connection.query('SELECT * FROM bamazon.products', function (error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results);
-    console.log('Number of rows: ' + results.length);
+    for (let i = 0; i < results.length; i++) {
+      console.log(`ID: ${results[i].item_id} - Name: ${results[i].product_name} - Cost: \$${results[i].price}`)
+    }
+    console.log('------------------------------------------------------------------');
+    return results;
   });
+}
 
-connection.end();
+function buyProducts() {
+  let results = printAll();
+  console.log(results)
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What ID would you like to purchase?'
+    },
+    {
+      type: 'input',
+      name: 'amount',
+      message: 'How many do you want to purchase?'
+    }
+  ]).then(function (answer) {
+    if ((results[answer.id].stock_quantity - answer.amount) >= 0) {
+      console.log(results[answer.id].stock_quantity);
+      console.log('Purchase has been processed!');
+    } else {
+      console.log('Insufficent stock');
+    }
+  });
+  connection.end();
+}
